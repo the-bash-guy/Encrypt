c87e900486562231d2d444918ed513dc5ad8f902
mosfet <68642808+DogLife2007@users.noreply.github.com>
2021-12-29 01:20:45 +0100

    Add files via upload

---
+++ ProgressBar/ProgressBar/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ProgBar = new System.Windows.Forms.ProgressBar();
+            this.elapsed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ProgBar
+            // 
+            this.ProgBar.Location = new System.Drawing.Point(13, 13);
+            this.ProgBar.Name = "ProgBar";
+            this.ProgBar.Size = new System.Drawing.Size(559, 36);
+            this.ProgBar.TabIndex = 0;
+            this.ProgBar.Click += new System.EventHandler(this.ProgBar_Click);
+            // 
+            // elapsed
+            // 
+            this.elapsed.AutoSize = true;
+            this.elapsed.Location = new System.Drawing.Point(10, 69);
+            this.elapsed.Name = "elapsed";
+            this.elapsed.Size = new System.Drawing.Size(0, 13);
+            this.elapsed.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 91);
+            this.Controls.Add(this.elapsed);
+            this.Controls.Add(this.ProgBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Progress";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar ProgBar;
+        private System.Windows.Forms.Label elapsed;
+    }
+}
+
---
+++ ProgressBar/ProgressBar/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public static string[] files;
+        public static int size1;
+        public static int size2;
+        public static int percent;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgBar_Click(object sender, EventArgs e)
+        {
+            string credits = "Creator: https://github.com/DogLife2007 \nOfficial Site: https://noescape1151.ddns.net";
+            MessageBox.Show(credits);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            files = Environment.GetCommandLineArgs();
+            long insize = (new FileInfo(files[1]).Length / 1024) / 1024;
+            size1 = (int)insize;
+            this.ProgBar.Maximum = size1;
+            Thread timer = new Thread(() => Elapsed_time());
+            Thread progress = new Thread(() => Progress());
+            timer.Start();
+            progress.Start();
+        }
+
+        private void Progress()
+        {
+            while (this.ProgBar.Value < (size1 - 0.2))
+            {
+                long outsize = (new FileInfo(files[2]).Length / 1024) / 1024;
+                size2 = (int)outsize;
+                this.ProgBar.Value = size2;
+            }
+            Application.Exit();
+        }
+
+        private void Elapsed_time()
+        {
+            Stopwatch time_elapsed = new Stopwatch();
+            time_elapsed.Start();
+            while (this.ProgBar.Value < (size1 - 0.2))
+            {
+                TimeSpan elapsedtime = time_elapsed.Elapsed;
+                this.elapsed.Text = "Elapsed time: " + elapsedtime.ToString(@"dd\.hh\:mm\:ss");
+            }
+            time_elapsed.Stop();
+            Application.Exit();
+        }
+    }
+}
